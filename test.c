4c4d8b1bdcfc7531bb63ecf9c92a7dd5cbfca07e
Anshul2207 <anshul2207@gmail.com>
2016-06-10 15:06:11 +0530

    for looping

--- test.c
+++ test.c
@@ -1,3 +1,7 @@
 //test file
 edit this file ----- 1st June, 2016
 this file is edited on 8th juen for weekly exeuction testing by Anshul Jain
+for (i=0;i<=10;i++)
+{
+  this is the test to confirm jenkins CI + i;
+}
